6cb34fefb9cd049478ebfe60e79d679ab134384d
abdirizak abdullahi <122808527+zack8899@users.noreply.github.com>
2023-07-09 07:32:50 +0300

    Add files via upload

---
+++ src/spaghetti.js
@@ -0,0 +1,7 @@
+const classList = document.getElementById('navMenue')
+
+console.log('hi')
+document.getElementById('burger').onclick = ()=>{
+    classList.classList.toggle('hidden')
+    classList.classList.toggle('flex')
+}
